da84b85a91d2c83e31d7855b77734560fecbc554
norgeous <daniel@harte.me>
2024-11-05 16:49:24 +0000

    Update main.ts

--- src/game/main.ts
+++ src/game/main.ts
@@ -5,7 +5,7 @@ import { MainMenu } from "./scenes/MainMenu";
 import { AUTO, Game } from "phaser";
 import { Preloader } from "./scenes/Preloader";
 
-const VIEWPORT_SIZE = 40;
+const VIEWPORT_SIZE = 80;
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
