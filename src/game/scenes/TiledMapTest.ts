a66c81765923d192c772800aab39ec9c58876b64
scott manning <scotty6663@hotmail.com>
2024-11-07 20:08:00 +0000

    adding test map file

--- src/game/scenes/TiledMapTest.ts
+++ src/game/scenes/TiledMapTest.ts
@@ -3,7 +3,7 @@ import { Scene } from "phaser";
 import TiledMapBuilder, { LevelConfigType } from '../../objects/map/TiledMapBuilder';
 
 const levelConfig: LevelConfigType = {
-    key: 'level2',
+    key: 'Room',
     tilesetPng: './tiled/tileset/binding_of_isaac_tiles.jpg',
     tiledMapJson: './tiled/maps/test.json',
     layerConfig: [
@@ -20,21 +20,14 @@ export class TiledMapTest extends Scene {
     constructor() {
         super("TiledMapTest");
     }
-      
+    
     preload() {
-        //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
-        //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-
         TiledMapBuilder.preload(this, levelConfig);
-        
-        // this.load.image("background", "assets/bg.png");
     }
     
     create() {
-        EventBus.emit("current-scene-ready", this);
-
         this.map = new TiledMapBuilder(this, levelConfig);
-
+        EventBus.emit("current-scene-ready", this);
     }  
 
     changeScene() {
