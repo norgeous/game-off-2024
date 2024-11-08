fb42fa063e2d2ea61b7fc209d00cf6227cb19a3b
norgeous <daniel@harte.me>
2024-11-08 14:57:23 +0000

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react';
 import { FaCog, FaImages } from 'react-icons/fa';
 
-const Menu = () => {
+const sceneNames = ['MainMenu', 'Game', 'GameOver', 'Win', 'TiledMapTest'];
+const SceneSelector = ({ phaserScene }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <button onClick={() => setIsOpen(!isOpen)}><FaCog /></button>
+      <button onClick={() => setIsOpen(!isOpen)}>Current scene: {phaserScene?.scene.key} <FaImages /></button>
       {isOpen && (
         <>
-          <button><FaImages /></button>
+          {sceneNames.map(sceneName => (
+            <button
+              onClick={() => phaserScene?.scene.start(sceneName)}
+            >
+              {sceneName}
+            </button>
+          ))}
+        </>
+      )}
+    </>
+  )
+};
+
+const Menu = ({ phaserScene }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+
+      {import.meta.env.PROD ? 'isProd':'isNotProd'}
+      <button onClick={() => setIsOpen(!isOpen)}>Settings <FaCog /></button>
+      {isOpen && (
+        <>
+          <SceneSelector phaserScene={phaserScene}/>
         </>
       )}
     </>
