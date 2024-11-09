1b080b3bb874046beea42a2cabaf64a091b90e83
scott manning <scotty6663@hotmail.com>
2024-11-09 19:33:26 +0000

    'linting'

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,5 +1,5 @@
-import { Scene } from "phaser";
-import { loadRandomMapData } from "../../objects/map/Map";
+import { Scene } from 'phaser';
+import { loadRandomMapData } from '../../objects/map/Map';
 
 export class Preloader extends Scene {
   constructor() {
@@ -27,9 +27,9 @@ export class Preloader extends Scene {
     //  Load the assets for the game - Replace with your own assets
     this.load.setPath('assets');
 
-    this.load.image("player", "jones.png");
-    this.load.image("logo", "logo.png");
-    this.load.image("star", "star.png");
+    this.load.image('player', 'jones.png');
+    this.load.image('logo', 'logo.png');
+    this.load.image('star', 'star.png');
     this.load.json('mapData', 'map-data.json');
   }
 
@@ -37,6 +37,6 @@ export class Preloader extends Scene {
     //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
     //  For example, you can define global animations here, so we can use them in other scenes.
     loadRandomMapData(this);
-    this.scene.start("MainMenu");
+    this.scene.start('MainMenu');
   }
 }
--- src/game/scenes/Rooms.ts
+++ src/game/scenes/Rooms.ts
@@ -1,55 +1,57 @@
-import { EventBus } from "../EventBus";
-import Player from "../../objects/entities/Player";
-import { Scene } from "phaser";
-import TiledMapBuilder, { LevelConfigType } from '../../objects/map/TiledMapBuilder';
-import { buildRoomJsonPath, getNextRoomId, CurrentRoomId } from "../../objects/map/Map";
-import Direction from "../../enums/Direction";
+import { EventBus } from '../EventBus';
+import Player from '../../objects/entities/Player';
+import { Scene } from 'phaser';
+import TiledMapBuilder, {
+  LevelConfigType,
+} from '../../objects/map/TiledMapBuilder';
+import {
+  buildRoomJsonPath,
+  getNextRoomId,
+  CurrentRoomId,
+} from '../../objects/map/Map';
+import Direction from '../../enums/Direction';
 
 const levelConfig: LevelConfigType = {
-    key: 'room',
-    tilesetPng: './tiled/tileset/binding_of_isaac_tiles.jpg',
-    tiledMapJson: './tiled/maps/room-2.json',
-    layerConfig: [
-      { tiledLayerName: 'tiledLayer', depth: 10 },
-    ],
-    spawnerConfig: [],
+  key: 'room',
+  tilesetPng: './tiled/tileset/binding_of_isaac_tiles.jpg',
+  tiledMapJson: './tiled/maps/room-2.json',
+  layerConfig: [{ tiledLayerName: 'tiledLayer', depth: 10 }],
+  spawnerConfig: [],
 };
 
 export class Rooms extends Scene {
-    public map: TiledMapBuilder | undefined;
+  public map: TiledMapBuilder | undefined;
 
-    public player: Player;
+  public player: Player;
 
-    public sprite: Phaser.GameObjects.Sprite
+  public sprite: Phaser.GameObjects.Sprite;
 
-    public levelConfig: LevelConfigType;
+  public levelConfig: LevelConfigType;
 
-    constructor() {
-        super("Rooms");
+  constructor() {
+    super('Rooms');
+  }
+
+  init(direction: Direction) {
+    if (direction) {
+      getNextRoomId(direction);
     }
-    
-    init(direction: Direction) {
-        if (direction) {
-            getNextRoomId(direction);
-        }
-    }
-    
-    preload() {
-        levelConfig.key = 'room-' + CurrentRoomId; 
-        levelConfig.tiledMapJson = buildRoomJsonPath();
-        TiledMapBuilder.preload(this, levelConfig);
-    }
-    
-    create() {
-        this.map = new TiledMapBuilder(this, levelConfig);
-        EventBus.emit("current-scene-ready", this);
-    }  
+  }
 
-    update() {
-        
-    }
+  preload() {
+    levelConfig.key = 'room-' + CurrentRoomId;
+    levelConfig.tiledMapJson = buildRoomJsonPath();
+    TiledMapBuilder.preload(this, levelConfig);
+  }
 
-    changeScene() {
-        this.scene.start("GameOver");
-    }
+  create() {
+    this.map = new TiledMapBuilder(this, levelConfig);
+    EventBus.emit('current-scene-ready', this);
+  }
+
+  update() {}
+
+  changeScene() {
+    this.scene.start('GameOver');
+  }
 }
