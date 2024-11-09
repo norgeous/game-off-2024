8615dd4c92a1e541c8b2026f4a57f8ab691705c8
scott manning <scotty6663@hotmail.com>
2024-11-09 18:05:51 +0000

    adding Map object

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,6 +1,5 @@
 import { Scene } from "phaser";
-import Player from "../../objects/entities/Player";
-import { loadRandomMapData, setRandomStartingRoom } from "../../objects/map/MapLocation";
+import { loadRandomMapData } from "../../objects/map/Map";
 
 export class Preloader extends Scene {
   constructor() {
@@ -31,17 +30,13 @@ export class Preloader extends Scene {
     this.load.image("player", "jones.png");
     this.load.image("logo", "logo.png");
     this.load.image("star", "star.png");
-    this.load.json('mapData', 'Maps.json');
+    this.load.json('mapData', 'map-data.json');
   }
 
   create() {
     //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
     //  For example, you can define global animations here, so we can use them in other scenes.
-
     loadRandomMapData(this);
-    setRandomStartingRoom(this);
-
-    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
     this.scene.start("TiledMapTest");
   }
 }
