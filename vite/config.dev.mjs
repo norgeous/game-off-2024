b44d1a1b38b783962dbfe59752059c91f4c1dd09
norgeous <daniel@harte.me>
2024-11-09 15:50:12 +0000

    add pwa to dev too

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import pwaConfig from './pwaConfig';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(pwaConfig),
+  ],
   server: {
     port: 8080,
   },
