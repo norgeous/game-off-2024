9862d3ba652039e561e7740a6afeea3496122050
norgeous <daniel@harte.me>
2024-11-09 01:08:09 +0000

    fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useRef, useState } from "react";
 import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
-import Button from "./components/Button";
-import Container from "./components/Container";
-import Navigation from "./enums/Navigation";
 import Menu from "./components/Menu";
 
 function App() {
@@ -16,25 +13,6 @@ function App() {
     <div id="app">
       <Menu phaserScene={phaserScene} />
       <PhaserGame ref={phaserRef} onChangeScene={onChangeScene} />
-      <Container>
-        scene: {sceneKey}
-        <Button onClick={changeScene}>Change Scene</Button>
-        <Button onClick={() => phaserScene?.scene.start("Win")}>Win</Button>
-        <Button onClick={() => phaserScene?.scene.start("TiledMapTest")}>Tiled Test</Button>
-        <Button disabled={sceneKey !== "MainMenu"} onClick={moveSprite}>
-          Toggle Movement
-        </Button>
-        <div>
-          Sprite Position:
-          <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
-        </div>
-        <Button onClick={addSprite}>Add New Sprite</Button>
-          Debug Map Navigation
-          <Button onClick={() => roomNavigation(Navigation.UP)}>Room Up</Button>
-          <Button onClick={() => roomNavigation(Navigation.DOWN)}>Room Down</Button>
-          <Button onClick={() => roomNavigation(Navigation.LEFT)}>Room Left</Button>
-          <Button onClick={() => roomNavigation(Navigation.RIGHT)}>Room Right</Button>
-      </Container>
     </div>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
-import { FaCog, FaImages } from 'react-icons/fa';
-import { FaXmark } from 'react-icons/fa6';
+import { FaImages } from 'react-icons/fa';
+import { FaGear, FaXmark, FaArrowUp, FaArrowDown, FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
 import styled from 'styled-components';
 import Container from './Container';
 import Modal from './Modal';
+import Navigation from "../enums/Navigation";
 
 const Button = styled.button`
   background: none;
   color: inherit;
   border: none;
+  cursor: pointer;
 `;
 
 const sceneNames = ['MainMenu', 'Game', 'GameOver', 'Win', 'TiledMapTest'];
@@ -36,20 +38,29 @@ const SceneSelector = ({ phaserScene, isOpen, setIsOpen }) => {
   );
 };
 
+
 const Menu = ({ phaserScene }) => {
   const [isSettingsOpen, setSettingsIsOpen] = useState(false);
   const [isSceneSelectorOpen, setIsSceneSelectorOpen] = useState(false);
+
+  const roomNavigation = (direction: Navigation) => {
+    phaserScene?.scene.start('TiledMapTest', { roomId: 1 });
+  };
 
   return (
     <>
       <Container>
         <Button onClick={() => setSettingsIsOpen(!isSettingsOpen)}>
-          {isSettingsOpen ? <FaXmark size={32} /> : <FaCog size={32} /> }
+          {isSettingsOpen ? <FaXmark size={32} /> : <FaGear size={32} /> }
         </Button>
         {isSettingsOpen && (
           <>
             {/* {import.meta.env.PROD ? 'isProd' : 'isDev'} */}
             <Button onClick={() => setIsSceneSelectorOpen(!isSceneSelectorOpen)}><FaImages size={32} /></Button>
+            <Button onClick={() => roomNavigation(Navigation.UP)}><FaArrowUp size={32} /></Button>
+            <Button onClick={() => roomNavigation(Navigation.DOWN)}><FaArrowDown size={32} /></Button>
+            <Button onClick={() => roomNavigation(Navigation.LEFT)}><FaArrowLeft size={32} /></Button>
+            <Button onClick={() => roomNavigation(Navigation.RIGHT)}><FaArrowRight size={32} /></Button>
           </>
         )}
       </Container>
