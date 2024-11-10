72767bf4e6bb33ac1887c3c61c2d8c172b510d34
norgeous <daniel@harte.me>
2024-11-10 12:25:44 +0000

    typesafe?

--- src/helpers/dungeonConfigParser.ts
+++ src/helpers/dungeonConfigParser.ts
@@ -1,4 +1,43 @@
-const dungeonConfigTo2D = (dungeonConfig: string) =>
+type Direction = 'north' | 'south' | 'east' | 'west';
+
+export type DataForSceneType = {
+  roomType: string;
+  adjacentRooms: { [direction in Direction]: string };
+  playerEnterFrom: Direction;
+};
+
+type RoomConfig1D = {
+  x: number;
+  y: number;
+  roomType: string;
+};
+
+// Find the object in array by x and y keys
+// always returns a value, even when not found
+export const findRoomConfigByCoordinate = (
+  dungeon1D: RoomConfig1D[],
+  px: number,
+  py: number,
+) => {
+  const roomConfig = dungeon1D.find(({ x, y }) => x === px && y === py); // or undefined
+  if (!roomConfig) return { x: 0, y: 0, roomType: '?' };
+  else return roomConfig;
+};
+
+// get roomType for the coord
+const getRoomType = (dungeon1D: RoomConfig1D[], x: number, y: number) =>
+  findRoomConfigByCoordinate(dungeon1D, x, y)?.roomType;
+
+// get the adjacentRooms for the coord
+const getAdjacentRooms = (dungeon1D: RoomConfig1D[], x: number, y: number) => ({
+  north: getRoomType(dungeon1D, x, y - 1),
+  south: getRoomType(dungeon1D, x, y + 1),
+  east: getRoomType(dungeon1D, x + 1, y),
+  west: getRoomType(dungeon1D, x - 1, y),
+});
+
+// Convert the multiline dungeon string into a 1 dimensional array of RoomConfig1D objects
+const dungeonConfigTo1D = (dungeonConfig: string) =>
   dungeonConfig
     .trim()
     .split('\n')
@@ -7,53 +46,57 @@ const dungeonConfigTo2D = (dungeonConfig: string) =>
     )
     .flat();
 
-type RoomConfig = {
-  x: number;
-  y: number;
-  roomType: string;
-};
-
-export const findRoomConfigByCoordinate = (
-  arr: RoomConfig[],
-  px: number,
-  py: number,
-) => {
-  const roomConfig = arr.find(({ x, y }) => x === px && y === py); // or undefined
-  if (!roomConfig || roomConfig.roomType === '.') return undefined;
-  else return roomConfig;
-};
-
+// main function to convert dungeonStr to array of RoomConfig
 const dungeonConfigParser = (dungeonStr: string) => {
-  const flatted = dungeonConfigTo2D(dungeonStr);
-
-  const withDoors = flatted.map((roomConfig) => {
-    const { x, y } = roomConfig;
-
-    const adjacentRooms = {
-      north: findRoomConfigByCoordinate(flatted, x, y - 1)?.roomType,
-      south: findRoomConfigByCoordinate(flatted, x, y + 1)?.roomType,
-      east: findRoomConfigByCoordinate(flatted, x + 1, y)?.roomType,
-      west: findRoomConfigByCoordinate(flatted, x - 1, y)?.roomType,
-    };
-
-    return {
-      ...roomConfig,
-      adjacentRooms: roomConfig.roomType !== '.' ? adjacentRooms : {},
-    };
-  });
-
-  return withDoors;
+  const dungeon1D = dungeonConfigTo1D(dungeonStr);
+  return dungeon1D;
 };
-
 export default dungeonConfigParser;
 
-export const to2D = (dungeonConfig: RoomConfig[]) =>
-  Object.values(Object.groupBy(dungeonConfig, ({ y }: { y: number }) => y));
+// when moving to next scene, this will get the config data of the next room
+export const getNextRoom = (
+  dungeon1D: RoomConfig1D[],
+  currentX: number,
+  currentY: number,
+  direction: Direction,
+) => {
+  // convert current position and direction into new position
+  const { x, y, playerEnterFrom } = {
+    north: {
+      x: currentX,
+      y: currentY - 1,
+      playerEnterFrom: 'south',
+    },
+    south: {
+      x: currentX,
+      y: currentY + 1,
+      playerEnterFrom: 'north',
+    },
+    east: {
+      x: currentX + 1,
+      y: currentY,
+      playerEnterFrom: 'west',
+    },
+    west: {
+      x: currentX - 1,
+      y: currentY,
+      playerEnterFrom: 'east',
+    },
+  }[direction];
 
-type Direction = 'north' | 'south' | 'east' | 'west';
+  const roomType = getRoomType(dungeon1D, x, y);
+  const adjacentRooms = getAdjacentRooms(dungeon1D, x, y);
 
-export type DataForSceneType = {
-  roomType: string;
-  adjacentRooms: { [direction in Direction]: string };
-  playerEnterFrom: Direction;
+  return {
+    x,
+    y,
+    playerEnterFrom,
+    roomType,
+    adjacentRooms,
+  };
 };
+
+// convert the 1D RoomConfig array to a 2D nested array / matrix
+// this only is used for rendering the minimap
+export const to2D = (dungeonConfig: RoomConfig1D[]) =>
+  Object.values(Object.groupBy(dungeonConfig, ({ y }: RoomConfig1D) => y));
