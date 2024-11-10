f8b4db39a588242a2bc299bc1450d197090f4db5
norgeous <daniel@harte.me>
2024-11-10 13:06:45 +0000

    Update DungeonStateProvider.tsx

--- src/contexts/DungeonStateProvider.tsx
+++ src/contexts/DungeonStateProvider.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import DungeonStateContext from './DungeonStateContext';
-import dungeonState from '../helpers/dungeonState';
+// import createDungeonState from '../helpers/dungeonState';
+import { EventBus } from '../game/EventBus';
 
 const useDungeonState = () => {
+  const [reactDungeonState, setReactDungeonState] = useState();
+
+  useEffect(() => {
+    // const dungeonState = createDungeonState(setReactDungeonState); // create singleton
+
+    EventBus.on('current-scene-ready', (scene: Phaser.Scene) => {
+      console.log('current-scene-ready', scene);
+    });
+    return () => {
+      EventBus.removeListener('current-scene-ready');
+    };
+  }, []);
+
   return {
     'i am context': 'hello',
-    dungeonState,
+    reactDungeonState,
+    setReactDungeonState,
+    // dungeonState,
   };
 };
 
