53476932921b6bb005fdc4bcf377185eeff720cd
scott manning <scotty6663@hotmail.com>
2024-11-12 20:23:30 +0000

    add some enemies

---
+++ src/objects/entities/mobs/Bat.ts
@@ -0,0 +1,36 @@
+import * as Phaser from 'phaser';
+import Entity, { EntityConfigType } from '../Entity';
+import GameScene from '@/scenes/GameScene';
+import { CC, CM } from '../../../enums/CollisionCategories';
+
+const KEY = 'bat';
+
+const entityConfig: EntityConfigType = {
+  name: KEY,
+  collisionCategory: CC.enemy,
+  collisionMask: CM.enemy,
+  spriteSheetKey: KEY,
+  facing: -1,
+  scale: 0.6,
+  craftpixOffset: {
+    x: 0,
+    y: 0,
+  },
+  physicsConfig: {
+    width: 100,
+    height: 100,
+  },
+  animations: [
+  ],
+};
+
+class Bat extends Entity {
+  static preload(scene: Phaser.Scene) {
+    scene.load.image(KEY, 'assets/mobs/bat.png');
+  }
+  constructor(scene: GameScene, x: number, y: number) {
+    super(scene, x, y, entityConfig);
+  }
+}
+
+export default Bat;
---
+++ src/objects/entities/mobs/Snake.ts
@@ -0,0 +1,39 @@
+import * as Phaser from 'phaser';
+import Entity, { EntityConfigType } from '../Entity';
+import GameScene from '@/scenes/GameScene';
+import { CC, CM } from '../../../enums/CollisionCategories';
+
+const KEY = 'snake';
+
+const entityConfig: EntityConfigType = {
+  name: KEY,
+  collisionCategory: CC.enemy,
+  collisionMask: CM.enemy,
+  spriteSheetKey: KEY,
+  facing: -1,
+  scale: 0.6,
+  craftpixOffset: {
+    x: 0,
+    y: 0,
+  },
+  physicsConfig: {
+    width: 100,
+    height: 100,
+  },
+  animations: [
+  ],
+};
+
+class Snake extends Entity {
+  static preload(scene: Phaser.Scene) {
+    scene.load.image(KEY, 'assets/mobs/snake.png');
+  }
+  constructor(scene: GameScene, x: number, y: number) {
+    super(scene, x, y, entityConfig);
+    this.sprite.setDepth(100);
+    
+    this.setScale(0.3);
+  }
+}
+
+export default Snake;
