4924e60805523c79c3886489ef655c1e4289d30b
scott manning <scotty6663@hotmail.com>
2024-11-12 20:24:06 +0000

    random enemy function

---
+++ src/helpers/getRandomEnemy.ts
@@ -0,0 +1,15 @@
+import Bat from "../objects/entities/mobs/Bat";
+import Scorpion from "../objects/entities/mobs/Scorpion";
+import Snake from "../objects/entities/mobs/Snake";
+import { DataForSceneType } from "./dungeonConfigParser";
+
+const enemies = [
+    Bat,
+    Snake,
+    Scorpion,
+];
+
+export const getRandomEnemy = (dataForScene?: DataForSceneType) => {
+    //  We can spawn enemies based off dataForScene var.
+    return enemies[Math.floor(Math.random() * enemies.length)];
+} 
