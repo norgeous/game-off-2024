45753705a288b7a7adc976ddadc98ff58bddd6c8
norgeous <daniel@harte.me>
2024-11-13 18:09:30 +0000

    add mobile WASD

--- src/helpers/controls.ts
+++ src/helpers/controls.ts
@@ -1,7 +1,39 @@
 export type keysType = { [keyCodes: string]: Phaser.Input.Keyboard.Key };
 
-export const createControls = (scene: Phaser.Scene) =>
-  scene.input.keyboard?.addKeys('W,A,S,D') as keysType;
+const DEADZONE = 30;
+
+export const createControls = (scene: Phaser.Scene) => {
+  const mouseKeys = {
+    W: { isDown: false },
+    A: { isDown: false },
+    S: { isDown: false },
+    D: { isDown: false },
+  };
+  scene.input.on('pointerup', (d) => {
+    mouseKeys.W.isDown = false;
+    mouseKeys.A.isDown = false;
+    mouseKeys.S.isDown = false;
+    mouseKeys.D.isDown = false;
+  });
+  scene.input.on('pointermove', (d) => {
+    if (d.buttons === 1) {
+      const {
+        downX,
+        downY,
+        position: { x, y },
+      } = d;
+      const dx = x - downX;
+      const dy = y - downY;
+      mouseKeys.W.isDown = dy < -DEADZONE;
+      mouseKeys.A.isDown = dx < -DEADZONE;
+      mouseKeys.S.isDown = dy > DEADZONE;
+      mouseKeys.D.isDown = dx > DEADZONE;
+    }
+  });
+
+  if(scene.sys.game.device.os.desktop) return scene.input.keyboard?.addKeys('W,A,S,D') as keysType;
+  else return mouseKeys;
+};
 
 export const keysToVector = (keys: keysType | undefined, power: number) => {
   const vector = { x: 0, y: 0 };
