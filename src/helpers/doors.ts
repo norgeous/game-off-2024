996991bd68c6b016a373a94842b5e2c46b6a5ca1
norgeous <daniel@harte.me>
2024-11-13 20:42:58 +0000

    Update doors.ts

--- src/helpers/doors.ts
+++ src/helpers/doors.ts
@@ -3,9 +3,11 @@ import { TiledMapTest2 } from '../game/scenes/TiledMapTest2';
 
 const createDoors = (scene: TiledMapTest2) => {
   // console.log(scene);
+  const { width, height } = scene.map.layers.tiledLayer.defaultPipeline;
+  console.log({ width, height });
 
   if (!['%', '.'].includes(scene.sceneInitParams?.adjacentRooms?.north)) {
-    const doorNorth = scene.matter.add.sprite(500, 100, 'door', '0', {
+    const doorNorth = scene.matter.add.sprite(width*.5, height*.1, 'door', '0', {
       isStatic: true,
     });
     scene.player.setOnCollideWith(doorNorth, () =>
@@ -14,7 +16,7 @@ const createDoors = (scene: TiledMapTest2) => {
   }
 
   if (!['%', '.'].includes(scene.sceneInitParams?.adjacentRooms?.south)) {
-    const doorSouth = scene.matter.add.sprite(500, 800, 'door', '0', {
+    const doorSouth = scene.matter.add.sprite(width*.5, height*.9, 'door', '0', {
       isStatic: true,
     });
     doorSouth.setAngle(180);
@@ -24,7 +26,7 @@ const createDoors = (scene: TiledMapTest2) => {
   }
 
   if (!['%', '.'].includes(scene.sceneInitParams?.adjacentRooms?.east)) {
-    const doorEast = scene.matter.add.sprite(800, 500, 'door', '0', {
+    const doorEast = scene.matter.add.sprite(width*.935, height*.5, 'door', '0', {
       isStatic: true,
     });
     doorEast.setAngle(90);
@@ -34,7 +36,7 @@ const createDoors = (scene: TiledMapTest2) => {
   }
 
   if (!['%', '.'].includes(scene.sceneInitParams?.adjacentRooms?.west)) {
-    const doorWest = scene.matter.add.sprite(100, 500, 'door', '0', {
+    const doorWest = scene.matter.add.sprite(width*.065, height*.5, 'door', '0', {
       isStatic: true,
     });
     doorWest.setAngle(270);
