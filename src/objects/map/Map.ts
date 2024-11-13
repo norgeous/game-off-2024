c0f14cd3fad7d691f5633fbda90cfb60dd820ec5
norgeous <daniel@harte.me>
2024-11-13 13:12:33 +0000

    Update Map.ts

--- src/objects/map/Map.ts
+++ src/objects/map/Map.ts
@@ -3,12 +3,12 @@ import { Scene } from 'phaser';
 const BASE_MAP_PATH = './tiled/maps/room-';
 
 export let mapIndex: number;
-export let currentRoomId: string = '0';
+export let currentRoomId: number = 0;
 export let currentRoomIndex: number;
 export let mapData: Array<number>;
 export let startingRoomData: Array<number>;
 
-export const setCurrentRoomId = (id: string) => {
+export const setCurrentRoomId = (id: number) => {
   currentRoomId = id;
 };
 
