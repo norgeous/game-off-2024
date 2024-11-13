6cb0b2e1ac55b135b03447a39983994132412c9a
norgeous <daniel@harte.me>
2024-11-13 12:46:56 +0000

    player start near entry door

--- src/game/scenes/TiledMapTest2.ts
+++ src/game/scenes/TiledMapTest2.ts
@@ -1,6 +1,9 @@
 import { EventBus, EventNames } from '../EventBus';
 import { Scene } from 'phaser';
-import { SceneInitParamsType } from '../../helpers/dungeonConfigParser';
+import {
+  Direction,
+  SceneInitParamsType,
+} from '../../helpers/dungeonConfigParser';
 import TiledMapBuilder, {
   LevelConfigType,
 } from '../../objects/map/TiledMapBuilder';
@@ -15,6 +18,14 @@ const levelConfig: LevelConfigType = {
   layerConfig: [{ tiledLayerName: 'tiledLayer', depth: 0 }],
   spawnerConfig: [],
 };
+
+const getPlayerStartPosition = (playerEnterFrom: Direction) =>
+  ({
+    north: { px: 500, py: 200 },
+    south: { px: 500, py: 700 },
+    east: { px: 700, py: 500 },
+    west: { px: 200, py: 500 },
+  })[playerEnterFrom];
 
 export class TiledMapTest2 extends Scene {
   public sceneInitParams: SceneInitParamsType;
@@ -54,8 +65,13 @@ export class TiledMapTest2 extends Scene {
   create() {
     console.log('TiledMapTest2 scene got', this.sceneInitParams);
 
+    const { playerEnterFrom } = this.sceneInitParams;
+
     this.map = new TiledMapBuilder(this, levelConfig);
-    this.player = this.matter.add.sprite(500, 500, 'star');
+
+    const { px, py } = getPlayerStartPosition(playerEnterFrom);
+
+    this.player = this.matter.add.sprite(px, py, 'star');
     this.cameras.main.startFollow(this.player);
 
     createDoors(this); // must be called after player is created
