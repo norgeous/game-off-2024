d6da0c46fa190950ad10dd5188eb2b6a0ee62d0d
norgeous <daniel@harte.me>
2024-11-15 20:18:00 +0000

    Update Coins.tsx

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -20,14 +20,14 @@ const Coins = () => {
   return (
     <>
       <Container>
+        <FaCoins size={32} style={{ color: 'gold' }} />
+        <CoinCount>{display}</CoinCount>
+      </Container>
+      <Container>
         <button onClick={() => setCount(0)}>0</button>
         <button onClick={() => setCount(count - 1)}>-1</button>
         <button onClick={() => setCount(count + 1)}>+1</button>
         <button onClick={() => setCount(count + 1_000_000)}>+1M</button>
-      </Container>
-      <Container>
-        <FaCoins size={32} style={{ color: 'gold' }} />
-        <CoinCount>{display}</CoinCount>
       </Container>
     </>
   );
