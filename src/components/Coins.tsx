27c096f0596b7dd23d809bde87061012f3c94bcd
norgeous <daniel@harte.me>
2024-11-15 18:04:38 +0000

    Update Coins.tsx

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -2,7 +2,7 @@ import { FaCoins } from 'react-icons/fa6';
 
 const Coins = () => (
   <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
-    <FaCoins size={32} /> 10,000
+    <FaCoins size={32} /> 10,000 <div style={{ fontSize: 32 }}>𓀀</div>
   </div>
 );
 
