a2b229d9f32c3f2ea889a15ebb9f81be70ead4c1
norgeous <daniel@harte.me>
2024-11-15 18:04:34 +0000

    Update Health.tsx

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -5,18 +5,20 @@ const Health = () => {
   const [playerHealth, setPlayerHealth] = useState(3);
   return (
     <>
-    <div style={{ display: 'flex', gap: 8, color: 'firebrick' }}>
-      {Array.from({ length: playerHealth }, (_,i) => <FaHeart key={i} size={32} />)}
-    </div>
-    <div>
-      <button onClick={() => setPlayerHealth(playerHealth + 1)}>
-        add heart
-      </button>
-      <button onClick={() => setPlayerHealth(playerHealth - 1)}>
-        remove heart
-      </button>
-        </div>
-      </>
+      <div style={{ display: 'flex', gap: 8, color: 'firebrick' }}>
+        {Array.from({ length: playerHealth }, (_, i) => (
+          <FaHeart key={i} size={32} />
+        ))}
+      </div>
+      <div>
+        <button onClick={() => setPlayerHealth(playerHealth + 1)}>
+          add heart
+        </button>
+        <button onClick={() => setPlayerHealth(playerHealth - 1)}>
+          remove heart
+        </button>
+      </div>
+    </>
   );
 };
 
