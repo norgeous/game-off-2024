1bc5d64da1e541a2538372a618b4d0071deb93b7
norgeous <daniel@harte.me>
2024-11-15 17:47:28 +0000

    Update Health.tsx

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -4,14 +4,19 @@ import { FaHeart } from 'react-icons/fa6';
 const Health = () => {
   const [playerHealth, setPlayerHealth] = useState(3);
   return (
+    <>
     <div style={{ display: 'flex', gap: 8, color: 'firebrick' }}>
-      {Array.from({ length: playerHealth }).map(() => (
-        <FaHeart size={32} />
-      ))}
+      {Array.from({ length: playerHealth }, (_,i) => <FaHeart key={i} size={32} />)}
+    </div>
+    <div>
       <button onClick={() => setPlayerHealth(playerHealth + 1)}>
         add heart
       </button>
-    </div>
+      <button onClick={() => setPlayerHealth(playerHealth - 1)}>
+        remove heart
+      </button>
+        </div>
+      </>
   );
 };
 
