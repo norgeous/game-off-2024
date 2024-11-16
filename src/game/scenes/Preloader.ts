5ae5d65f83a8c5287db341557db75b17e5f0ed10
norgeous <daniel@harte.me>
2024-11-16 16:30:33 +0000

    Update Preloader.ts

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -7,14 +7,24 @@ export class Preloader extends Scene {
   }
 
   init() {
+    const { width, height } = this.sys.game.canvas;
+
     //  We loaded this image in our Boot Scene, so we can display it here
-    this.add.image(512, 384, 'background');
+    this.add.image(width * 0.5, height * 0.5, 'background');
 
     //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    this.add
+      .rectangle(width * 0.5, height * 0.5, 468, 32)
+      .setStrokeStyle(1, 0xffffff);
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+    const bar = this.add.rectangle(
+      width * 0.5 - 230,
+      height * 0.5,
+      4,
+      28,
+      0xffffff,
+    );
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
     this.load.on('progress', (progress: number) => {
