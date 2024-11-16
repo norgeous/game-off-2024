a81541a017f7a8f9b807d6883cfac21f22361d88
norgeous <daniel@harte.me>
2024-11-16 10:59:45 +0000

    Update Health.tsx

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { FaHeart } from 'react-icons/fa6';
+import PlayerContext from '../contexts/PlayerContext';
 
 const Health = () => {
-  const [playerHealth, setPlayerHealth] = useState(3);
+  const { health, adjustHealth } = useContext(PlayerContext);
   return (
     <>
       <div style={{ display: 'flex', gap: 8, color: 'firebrick' }}>
-        {Array.from({ length: playerHealth }, (_, i) => (
+        {Array.from({ length: health }, (_, i) => (
           <FaHeart key={i} size={32} />
         ))}
       </div>
       <div>
-        <button onClick={() => setPlayerHealth(playerHealth + 1)}>
-          add heart
-        </button>
-        <button onClick={() => setPlayerHealth(playerHealth - 1)}>
-          remove heart
-        </button>
+        <button onClick={() => adjustHealth(+1)}>add heart</button>
+        <button onClick={() => adjustHealth(-1)}>remove heart</button>
       </div>
     </>
   );
