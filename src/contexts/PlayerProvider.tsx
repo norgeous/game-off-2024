ed0d2a0b2929d7cb4e7b46d81037c45da9000a92
norgeous <daniel@harte.me>
2024-11-16 10:49:27 +0000

    Update PlayerProvider.tsx

--- src/contexts/PlayerProvider.tsx
+++ src/contexts/PlayerProvider.tsx
@@ -1,12 +1,25 @@
 import { useState, useEffect } from 'react';
 import PlayerContext from './PlayerContext';
+import { EventBus, EventNames } from '../game/EventBus';
 
 const usePlayer = () => {
   const [health, setHealth] = useState(100);
+  const [inventory, setInventory] = useState([]);
+
+  // when the player takes damage
+  useEffect(() => {
+    EventBus.on(EventNames.ADJUST_PLAYER_HEALTH, (amount: number) => {
+      setHealth(health + amount);
+    });
+    return () => {
+      EventBus.removeListener(EventNames.ADJUST_PLAYER_HEALTH);
+    };
+  }, [health]);
 
   return {
-    health: 100,
-    inventory: [],
+    health,
+    inventory,
+    setInventory,
   };
 };
 
