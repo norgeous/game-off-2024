495f5d3b0c5eb9ca92a6a6aaeeff7d43725f05b9
scott manning <scotty6663@hotmail.com>
2024-11-17 22:40:55 +0000

    toggleMute changed to react button

--- src/components/MuteMusicToggle.tsx
+++ src/components/MuteMusicToggle.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FaMusic } from 'react-icons/fa6';
 import { FaVolumeMute } from 'react-icons/fa';
+import MenuButton from './MenuButton';
 
 interface IMuteMusicToggle {
   phaserScene: Phaser.Scene;
@@ -10,7 +11,7 @@ const MuteMusicToggle = ({ phaserScene }: IMuteMusicToggle) => {
   const [isMute, setIsMute] = useState(false);
 
   return (
-    <div
+    <MenuButton
       onClick={() => {
         phaserScene.sound.setMute(!isMute);
         setIsMute(!isMute);
@@ -18,7 +19,7 @@ const MuteMusicToggle = ({ phaserScene }: IMuteMusicToggle) => {
       style={{ cursor: 'pointer' }}
     >
       {isMute ? <FaVolumeMute size={30} /> : <FaMusic size={30} />}
-    </div>
+    </MenuButton>
   );
 };
 
