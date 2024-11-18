1ebe58b875344a116110fb0677bd5b7e03110e8b
scott manning <scotty6663@hotmail.com>
2024-11-18 22:57:52 +0000

    souundConfig => musicConfig

---
+++ src/helpers/getMusicConfig.ts
@@ -0,0 +1,50 @@
+import { AudioConfigType } from '../objects/Audio';
+import Audio from '../objects/Audio';
+
+const musicArray: AudioConfigType[] = [
+  {
+    key: 'music',
+    filePath: './audio/music/arab-and-muslim-190765.mp3',
+    loop: true,
+    isMusic: true,
+    roomType: '2',
+  },
+  {
+    key: 'music',
+    filePath: './audio/music/gypsy-animated-music-252548.mp3',
+    loop: true,
+    isMusic: true,
+    roomType: '2',
+  },
+  {
+    key: 'music',
+    filePath: './audio/music/punch-deck-brazilian-street-fight(chosic.com).mp3',
+    loop: true,
+    isMusic: true,
+    roomType: '2',
+  },
+  {
+    key: 'bossMusic',
+    filePath: './audio/music/the-queen-of-dunes-169012.mp3',
+    loop: true,
+    isMusic: true,
+    roomType: 'b',
+  },
+];
+
+const bossMusicArray = musicArray.filter(item => item.roomType === 'b');
+const roomMusicArray = musicArray.filter(item => item.roomType !== 'b');
+
+const getRandomMusicConfig = (musicConfigs: AudioConfigType[]) => {
+  return musicConfigs[Math.floor(Math.random() * musicConfigs.length)];
+};
+
+const roomMusic = getRandomMusicConfig(roomMusicArray);
+const bossMusic = getRandomMusicConfig(bossMusicArray);
+
+export const getCurrentRoomMusic = (roomType: string): AudioConfigType => {
+  return roomType === 'b' ? bossMusic : roomMusic;
+}
+
+export const musicConfig = [roomMusic, bossMusic];
+
