0d838d1f9d49ed06a6855bb14448355067e2e39b
norgeous <daniel@harte.me>
2024-11-19 22:19:15 +0000

    Update weapons.ts

--- src/helpers/weapons.ts
+++ src/helpers/weapons.ts
@@ -1,28 +1,33 @@
 import StarBullet from '../objects/weapons/bullets/StarBullet';
 
-const COOLDOWN = 400; // milliseconds between shots
-
 const inventory = ['star-gun', 'star-gun'];
 
 const itemName2Bullet = {
-  'star-gun': StarBullet,
+  'star-gun': {
+    Bullet: StarBullet,
+    cooldownLength: 1000, // milliseconds between shots
+  },
 };
 
 const weapons = (scene: Phaser.Scene) => {
-  const groups = inventory.map((itemName) => ({
-    cooldownFinish: 0,
-    group: scene.add.group({
-      maxSize: 100,
-      classType: itemName2Bullet[itemName],
-      runChildUpdate: true,
-    }),
-  }));
+  const groups = inventory.map((itemName) => {
+    const { Bullet, cooldownLength } = itemName2Bullet[itemName];
+    return {
+      cooldownLength,
+      cooldownFinishAt: 0,
+      group: scene.add.group({
+        maxSize: 100,
+        classType: Bullet,
+        runChildUpdate: true,
+      }),
+    };
+  });
 
   return (x: number, y: number, time: number) => {
-    groups.forEach(({ cooldownFinish, group }, index) => {
-      if (time > cooldownFinish) {
+    groups.forEach(({ cooldownLength, cooldownFinishAt, group }, index) => {
+      if (time > cooldownFinishAt) {
         group.get(x, y);
-        groups[index].cooldownFinish = time + COOLDOWN;
+        groups[index].cooldownFinishAt = time + cooldownLength; // reset cooldownFinish to some time in future
       }
     });
   };
