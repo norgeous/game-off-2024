5ac165d5ddb72cd02eb80bc20c338257074b132f
norgeous <daniel@harte.me>
2024-11-25 14:37:21 +0000

    Update TiledMapTest2.ts

--- src/scenes/TiledMapTest2.ts
+++ src/scenes/TiledMapTest2.ts
@@ -1,27 +1,14 @@
 import { EventBus, EventNames } from '../helpers/EventBus';
 import { Scene } from 'phaser';
 import { SceneInitParamsType } from '../helpers/dungeonConfigParser';
-// import TiledMapBuilder, {
-//   LevelConfigType,
-// } from '../objects/map/TiledMapBuilder';
-// import { getRandomEnemy } from '../helpers/getRandomEnemy';
 import createDoors from '../helpers/doors';
 import Player from '../objects/entities/Player';
 import { getCurrentRoomMusic } from '../helpers/getMusicConfig';
 import audio from '../objects/Audio';
 import { createRoom, preloadRoom } from '../rooms';
 
-// const levelConfig: LevelConfigType = {
-//   key: 'Room',
-//   tilesetPng: './tiled/tileset/ai-egypt-1.png',
-//   tiledMapJson: './tiled/maps/rooms/room-0.json',
-//   layerConfig: [{ tiledLayerName: 'tiledLayer', depth: 0 }],
-//   spawnerConfig: [],
-// };
-
 export class TiledMapTest2 extends Scene {
   public sceneInitParams: SceneInitParamsType;
-  // public map: TiledMapBuilder | undefined;
   public level: Phaser.Tilemaps.Tilemap | undefined;
   public spawners: { [k: string]: Phaser.GameObjects.Group };
   public player: Player;
@@ -38,19 +25,6 @@ export class TiledMapTest2 extends Scene {
   preload() {
     const { roomType } = this.sceneInitParams;
 
-    // levelConfig.spawnerConfig = [
-    //   {
-    //     tiledObjectName: 'enemy',
-    //     classFactory: getRandomEnemy(),
-    //     maxSize: 10,
-    //     runChildUpdate: true,
-    //     autoSpawn: true,
-    //   },
-    // // ];
-    // levelConfig.key = `room-${roomType}`;
-    // levelConfig.tiledMapJson = `./tiled/rooms/room-${roomType}.json`;
-    // TiledMapBuilder.preload(this, levelConfig);
-
     Player.preload(this);
 
     this.load.image('door', 'assets/isaac-door.png');
@@ -64,16 +38,18 @@ export class TiledMapTest2 extends Scene {
 
     console.log('TiledMapTest2 scene got', this.sceneInitParams, this);
 
+    // load tiled level
     const { level, spawners } = createRoom(this, roomType);
     this.level = level;
     this.spawners = spawners;
 
+    // setup audio
     audio.playRoomMusic(getCurrentRoomMusic(this.sceneInitParams.roomType).key);
     audio.setMusicMute(this.sceneInitParams.isMusicMuted);
 
+    // create player
     const { playerEnterFrom } = this.sceneInitParams;
     this.player = new Player(this, playerEnterFrom);
-    console.log(this.player.x);
 
     // camera constraint
     this.cameras.main
