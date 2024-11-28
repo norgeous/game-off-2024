9d38f5fb64948a75cc46a12340c2235516e0946e
scott manning <scotty6663@hotmail.com>
2024-11-28 13:22:13 +0000

    adding items

---
+++ src/objects/items/Heart.ts
@@ -0,0 +1,30 @@
+import { CC, CM } from "../../enums/CollisionCategories";
+import Item, { ItemConfigType } from "./Item";
+
+const KEY = 'heart';
+
+const config: ItemConfigType = {
+  key: KEY,
+  scale: 0.1,
+  collisionCategory: CC.item,
+  collisionMask: CM.item,
+  onPickUpCallBack: (_item, player) => {
+    player.updateStats({hp:1});
+  },
+};
+
+class Heart extends Item {
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+  ) {
+    super(scene, x, y, config);
+  }
+  
+  static preload(scene: Phaser.Scene) {
+    scene.load.image(KEY, 'assets/items/'+KEY+'.png');
+  }
+}
+
+export default Heart;
---
+++ src/objects/items/Item.ts
@@ -0,0 +1,88 @@
+import { CC, CM } from "../../enums/CollisionCategories";
+import Player from "../entities/Player";
+
+export type ItemConfigType = {
+  key: string;
+  scale: number;
+  collisionCategory: CC;
+  collisionMask: CM;
+  onPickUpCallBack?: (item: Item, entity: Player) => void;
+  onPickUpAnimation?: (item: Item) => void;
+};
+
+const defaultConfig: ItemConfigType = {
+  key: 'item',
+  scale: 1,
+  collisionCategory: CC.playerBullet,
+  collisionMask: CM.playerBullet,
+  onPickUpAnimation (item) {
+    item.scene.tweens.add({
+      targets: item.gameObject,
+      x: item.x,
+      y: item.y,
+      scaleX: 0.5, 
+      scaleY: 0.5,  
+      alpha: 0,    
+      duration: 500, 
+      ease: 'Power2',
+      onComplete: () => {
+        item.gameObject.destroy();
+      }
+    });
+  }
+};
+
+class Item extends Phaser.GameObjects.Container {
+  gameObject: Phaser.Physics.Matter.Sprite;
+  destroyOnPickUp: boolean;
+  sprite: Phaser.GameObjects.Sprite;
+  
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    config: ItemConfigType
+  ) {
+    super(scene, x, y);
+    const {
+      collisionCategory,
+      collisionMask,
+      key,
+      scale,
+      onPickUpAnimation,
+      onPickUpCallBack,
+    } = { ...defaultConfig, ...config };
+    
+    // Add sprite
+    this.gameObject = scene.matter.add
+       .sprite(x, y, key, undefined, {
+         collisionFilter: {
+           category: collisionCategory,
+           mask: collisionMask,
+         },
+       })
+       .setScale(scale)
+       .setStatic(true);
+      
+    this.gameObject.setOnCollide(
+      (data: Phaser.Types.Physics.Matter.MatterCollisionData) => {
+        if (data.bodyB?.collisionFilter.category === CC.player) {
+          onPickUpCallBack?.(this, data.bodyB.gameObject as Player);
+          scene.matter.world.remove(this.gameObject);
+          onPickUpAnimation?.(this);
+        }
+        if (data.bodyA?.collisionFilter.category === CC.player) {
+          onPickUpCallBack?.(this, data.bodyA.gameObject as Player);
+          scene.matter.world.remove(this.gameObject);
+          onPickUpAnimation?.(this);
+        }
+      },
+    );
+  }
+
+  update(_time: number, _delta: number) {
+    
+  }
+}
+
+export default Item;
