fc1f729b9dae0f99c5fc9b2c652b43a171fc9aa3
scott manning <scotty6663@hotmail.com>
2024-11-30 23:49:47 +0000

    adding hidden item room with machinegun

--- src/objects/weapons/bullets/WhipBullet.ts
+++ src/objects/weapons/bullets/WhipBullet.ts
@@ -1,12 +1,14 @@
 import { CC, CM } from '../../../enums/CollisionCategories';
 import Projectile, { ProjectileConfigType } from '../Projectile';
 
+const KEY = 'whip-bullet';
+
 const projectileConfig: ProjectileConfigType = {
-  key: 'whip',
-  assetName: 'whip.png',
+  key: KEY,
+  assetName: 'whip-bullet.png',
   scale: 2,
   stats: {
-    damage: 1,
+    damage: 0.5,
   },
   collisionCategory: CC.playerBullet,
   collisionMask: CM.playerBullet,
@@ -22,7 +24,7 @@ const projectileConfig: ProjectileConfigType = {
 
 class WhipBullet extends Projectile {
   static preload(scene: Phaser.Scene) {
-    scene.load.image('whip', 'assets/whip.png');
+    scene.load.image(KEY, 'assets/items/weapons/bullets/whip-bullet.png');
   }
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
