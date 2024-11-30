859e22353f50bfac4cbeeb4ada3a76f32d6a6e86
norgeous <daniel@harte.me>
2024-11-30 23:05:09 +0000

    Update ShopButtons.tsx

--- src/components/ShopButtons.tsx
+++ src/components/ShopButtons.tsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 import PlayerContext from '../contexts/PlayerContext';
+import ShopButton from './ShopButton';
+import CornerMenu, { Corner } from './CornerMenu';
 
 interface IShopButtons {
   phaserScene: Phaser.Scene;
@@ -10,79 +12,56 @@ const ShopButtons = ({ onClose }: IShopButtons) => {
   const { playerStats, updatePlayerStats } = useContext(PlayerContext);
 
   return (
-    <div style={{ position: 'absolute', bottom: 0 }}>
-      <div style={{ textAlign: 'right' }}>
-        hp:{' '}
-        <button onClick={() => updatePlayerStats({ hp: playerStats.hp - 1 })}>
-          -
-        </button>{' '}
-        {playerStats.hp}{' '}
-        <button onClick={() => updatePlayerStats({ hp: playerStats.hp + 1 })}>
-          +
-        </button>
-      </div>
-      <div style={{ textAlign: 'right' }}>
-        maxHp:{' '}
-        <button
-          onClick={() => updatePlayerStats({ maxHp: playerStats.maxHp - 1 })}
-        >
-          -
-        </button>{' '}
-        {playerStats.maxHp}{' '}
-        <button
-          onClick={() => updatePlayerStats({ maxHp: playerStats.maxHp + 1 })}
-        >
-          +
-        </button>
-      </div>
-      <div style={{ textAlign: 'right' }}>
-        speed:{' '}
-        <button
-          onClick={() =>
-            updatePlayerStats({
-              speed: Number((playerStats.speed - 0.1).toFixed(1)),
-            })
-          }
-        >
-          -
-        </button>{' '}
-        {playerStats.speed.toFixed(1)}{' '}
-        <button
-          onClick={() =>
-            updatePlayerStats({
-              speed: Number((playerStats.speed + 0.1).toFixed(1)),
-            })
-          }
-        >
-          +
-        </button>
-      </div>
-      <div style={{ textAlign: 'right' }}>
-        attackRate:{' '}
-        <button
-          onClick={() =>
-            updatePlayerStats({
-              attackRate: Number((playerStats.attackRate - 0.1).toFixed(1)),
-            })
-          }
-        >
-          -
-        </button>{' '}
-        {playerStats.attackRate.toFixed(1)}{' '}
-        <button
-          onClick={() =>
-            updatePlayerStats({
-              attackRate: Number((playerStats.attackRate + 0.1).toFixed(1)),
-            })
-          }
-        >
-          +
-        </button>
-      </div>
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          display: 'flex',
+          gap: 20,
+          padding: 12,
+        }}
+      >
+        <div>
+          <ShopButton
+            onClick={() => updatePlayerStats({ hp: playerStats.hp + 1 })}
+          >
+            Health +
+          </ShopButton>
+          <center>current: {playerStats.hp}</center>
+        </div>
 
-      <button onClick={() => onClose()}>Refund All</button>
-      <button onClick={() => onClose()}>Back</button>
-    </div>
+        <div>
+          <ShopButton
+            onClick={() =>
+              updatePlayerStats({
+                speed: Number((playerStats.speed + 0.1).toFixed(1)),
+              })
+            }
+          >
+            Speed +
+          </ShopButton>
+          <center>current: {playerStats.speed}</center>
+        </div>
+
+        <div>
+          <ShopButton
+            onClick={() =>
+              updatePlayerStats({
+                attackRate: Number((playerStats.attackRate + 0.1).toFixed(1)),
+              })
+            }
+          >
+            Attack Rate +
+          </ShopButton>
+          <center>current: {playerStats.attackRate}</center>
+        </div>
+      </div>
+      <CornerMenu $corner={Corner.BR}>
+        <button onClick={() => onClose()}>Refund All</button>
+        <button onClick={() => onClose()}>Back</button>
+      </CornerMenu>
+    </>
   );
 };
 
