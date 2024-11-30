86a1e1e4a192a3dc7a857fa7f941da17be28367a
norgeous <daniel@harte.me>
2024-11-30 21:36:22 +0000

    Update Coins.tsx

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -15,13 +15,23 @@ const CoinCount = styled.div`
   text-shadow: 2px 2px #000;
 `;
 
-const Coins = () => {
+interface ICoins {
+  phaserScene: Phaser.Scene;
+}
+
+const Coins = ({ phaserScene }: ICoins) => {
   const { coins } = useContext(PlayerContext);
   return (
     <>
       <Container>
         <FaCoins size={30} style={{ color: 'gold' }} />
         <CoinCount>{new Intl.NumberFormat().format(coins)}</CoinCount>
+
+        {phaserScene.scene.key === 'MainMenu' && (
+          <button onClick={() => phaserScene.scene.start('Shop')}>
+            Power Up!
+          </button>
+        )}
       </Container>
     </>
   );
