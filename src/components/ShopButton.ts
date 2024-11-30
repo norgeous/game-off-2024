3ee8041ce8e9ea399a27f0c23a1d0b67e445ace4
norgeous <daniel@harte.me>
2024-11-30 23:31:25 +0000

    Update ShopButton.ts

--- src/components/ShopButton.ts
+++ src/components/ShopButton.ts
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 
 const ShopButton = styled.button`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
   padding: 8px;
-  font-size: 24px;
-  width: 200px;
+  font-size: 22px;
+  width: 180px;
 `;
 
 export default ShopButton;
