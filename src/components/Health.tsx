0680b84652002e2b6f16b1dbe84e0e6a4ea135eb
norgeous <daniel@harte.me>
2024-11-30 23:52:22 +0000

    maxDisplayHearts

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -1,15 +1,28 @@
 import { useContext } from 'react';
 import PlayerContext from '../contexts/PlayerContext';
 
+const maxDisplayHearts = 6;
+
 const Health = () => {
   const {
     playerStats: { hp },
   } = useContext(PlayerContext);
+
   return (
-    <div style={{ display: 'flex', gap: 8, flexWrap: 'wrap' }}>
-      {Array.from({ length: hp }, (_, i) => (
+    <div
+      style={{
+        display: 'flex',
+        gap: 8,
+        flexWrap: 'wrap',
+        fontWeight: 'bold',
+        textShadow: '2px 2px #000',
+        fontSize: 30,
+      }}
+    >
+      {Array.from({ length: Math.min(hp, maxDisplayHearts) }, (_, i) => (
         <img key={i} src="./assets/items/heart.png" />
       ))}
+      {hp > maxDisplayHearts && `+${hp - maxDisplayHearts}`}
     </div>
   );
 };
